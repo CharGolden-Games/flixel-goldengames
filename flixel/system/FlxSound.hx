ed7ea0143329797a4c4c73a74b32ee9aa8e479f1
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-04-08 17:50:38 -0400

    re-add FlxSound redirect

---
+++ flixel/system/FlxSound.hx
@@ -0,0 +1,7 @@
+package flixel.system;
+
+/**
+ * FlxSound redirect for projects prior to 6.0.0
+ */
+@:deprecated("FlxSound has been moved to flixel.sound.FlxSound")
+typedef FlxSound = flixel.sound.FlxSound;
